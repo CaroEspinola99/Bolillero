b0c4b3c3df32548e8d118563511ad0effccb94eb
Cynthia <caroespinola1999@gmail.com>
2018-04-18 15:45:04 -0300

    Initial commit

---
+++ Bolillero/Simulacion.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPBolillero
+{
+    public class Simulacion
+    {
+        public static bool Acierta(Bolillero unBolillero, List<Byte> listita)
+        {
+            return listita.All(numero => numero == unBolillero.sacarBolillaAlAzar());
+        }
+
+        public static ulong VecesQueAcierta(Bolillero unBolillero, List<Byte> listita, ulong cantidadSimulacion)
+        {
+            ulong contador = 0;
+            for (ulong i = 0; i < cantidadSimulacion; i++)
+            {
+                unBolillero.volverAColocarBolillas();
+                if (Acierta(unBolillero, listita))
+                {
+                    contador++;
+                }
+            }
+            return contador;
+
+        }
+        public static ulong VecesQueAciertaConHilos(Bolillero unBolillero, List<Byte> listita, ulong cantidadSimulacion, Byte CantHilos)
+        {
+
+        }
+    }
+}
---
+++ PruebaBolillero/UnitTest1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using TPBolillero;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace PruebaBolillero
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        byte cantBolillas;
+        Bolillero bolillero;
+
+        [TestInitialize]
+        public void iniciar()
+        {
+            cantBolillas = 100;
+            bolillero = new Bolillero(cantBolillas);
+        }
+
+        [TestMethod]
+        public void cantidadDeBolillas()
+        {
+            Assert.AreEqual(cantBolillas, bolillero.BolillasAdentro.Count);
+            Assert.AreEqual(0, bolillero.BolillasFuera.Count);
+        }
+
+        [TestMethod]
+        public void efectoDelBolillero()
+        {
+            byte bolilla = bolillero.sacarBolillaAlAzar();
+            Assert.IsFalse(bolillero.BolillasAdentro.Contains(bolilla));
+            Assert.IsTrue(bolillero.BolillasFuera.Contains(bolilla));
+        }
+
+        [TestMethod]
+        public void probandoClon()
+        {
+            Bolillero otroBolillero = (Bolillero)bolillero.Clone();
+            Assert.AreEqual(otroBolillero.CantidadDeBolillas, bolillero.CantidadDeBolillas);
+            Assert.AreEqual(otroBolillero.BolillasAdentro.Count, bolillero.BolillasAdentro.Count);
+            Assert.IsTrue(otroBolillero.BolillasAdentro.TrueForAll(bolilla => bolillero.BolillasAdentro.Contains(bolilla)));
+            Assert.AreNotSame(bolillero, otroBolillero);
+            Assert.AreSame(bolillero, bolillero);
+        }
+        [TestMethod]
+
+        public void vecesQueAcierta()
+        {
+            List<Byte> listita = new List<Byte>{0};
+            Bolillero unBolillero = new Bolillero(1);
+            Assert.AreEqual((ulong)500000000, Simulacion.VecesQueAcierta(unBolillero, listita, 500000000));
+                
+        }
+
+    }
+}
